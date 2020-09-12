f7cdb832c7aa699962e94e69e631936293ceb569
Ricardo Arana Reyes <raranare@everis.com>
2020-09-12 15:53:49 -0500

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HolaMundo';
+  nombres = ['Ricardo', 'Martin'];
+  nombre: string;
+  mostrar: boolean;
+  indice = 0;
+
+  constructor() {
+    this.mostrar = false;
+    this.nombre = this.nombres[0];
+  }
+
+  cambiarNombre() {
+    this.indice = this.indice === 0 ? 1 : 0;
+    this.nombre =this.nombres[this.indice];
+  }
+
+  verMas() {
+    this.mostrar = true;
+  }
 }
